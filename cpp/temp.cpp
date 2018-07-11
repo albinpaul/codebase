37fddb53b0a25a81f6191ffff06218e4557a8253
albinpaul <albinpaul.ks@gmail.com>
2018-07-11 19:51:04 +0530

    asdf

---
+++ cpp/temp.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define MOD 1000000007
+#define forn(i, n) for (long i = 0; i < n; i++)
+#define mp make_pair
+#define pb push_back
+#define watch(x) cerr << (#x) << " is " << (x)<<" "
+#define debug() cerr << "running till line " << __LINE__ << " in function " << __func__ << endl
+const double PI = 3.141592;
+const long inf = (long)1.01e9;
+const double eps = (1e-9);
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<long> vl;
+typedef vector<long> vi;
+
+void solve();
+
+
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(0);
+#ifdef _DEBUG
+    long tt = clock();
+#endif
+
+    solve();
+
+#ifdef _DEBUG
+    cerr << "TIME = " << clock() - tt << endl;
+    tt = clock();
+#endif
+}
+
+
+void solve()
+{ 
+   
+
+
+}
