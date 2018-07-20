89891951d4b388a64d46a5f328a7159bb09fbe44
albinpaul <albinpaul.ks@gmail.com>
2018-07-20 10:05:33 +0530

    this it the message

--- codeforces/practice/randomprogams/test.py
+++ codeforces/practice/randomprogams/test.py
@@ -1,32 +1,18 @@
-import copy
-def ispalindrome(n):
-	string =str(n)
-	for i in range( int(len(string)/2) +1 ):
-		if string[i]!=string[len(string)-i-1]:
-			return False
-	return True
-
-def foo(n,index):
-	if index < 50:
+def s(n):
+	c=0
+	while(n):
+		c+=(n%10)
+		n=n//10
 		
-		g=str(n)
-		#print(g)
-		if( ispalindrome(int(g))):
-			return False
-		else:
-			return foo(int(g)+int(g[::-1]),index+1)
-	else:
-		return True
-cou=0
-for i in range(10001,0,-1):
-	g=str(i)
-	#print("i is ",i)
-	t=copy.deepcopy(g)
-	
-	if( ispalindrome(int(g) )):
-		pass
-	else:
-		if ( foo(int(g)+int(g[::-1]),1 ) ==True ):
-			print(i)
-			cou+=1
-print(cou)
+	#print(c)
+	return c
+
+m=0
+for a in range(100,0,-1):
+	for b in range(100,0,-1):
+		#print(a**b)
+		t=s(a**b)
+		if t>m:
+			print(a**b)
+			m=t
+print(m)
--- python/test.py
+++ python/test.py
@@ -1,1 +1,25 @@
-s={1,2,3}
+from pprint import pprint
+# with open('p059_cipher.txt','r') as f:
+#     content=f.read()
+#     arr =list(map(int,content.split(',')))
+#     #pprint(arr)
+#     for i in range(ord('a'),ord('z')+1):
+#         for j in range(ord('a'),ord('z')+1):
+#             for k in range(ord('a'),ord('z')+1):
+#                 temp =[0]*len(arr)
+#                 #print(i,j,k)
+#                 for index in range(0,len(arr),3):
+#                     if(index<len(arr)):
+#                         temp[index] = arr[index]^i
+#                     if(index+1<len(arr)):
+#                         temp[index+1] = arr[index+1]^j
+#                     if(index+2<len(arr)):
+#                         temp[index+2] = arr[index+2]^k
+#                 print(''.join(map(chr,temp)))
+
+with open('required.txt','r') as  f:
+    content=f.read()
+    s=0
+    for i in content:
+        s+=ord(i)
+    print (s)
