9222dbcaaa1931e0e5b9e6cf7916a831ac21a44b
albinpaul <albinpaul.ks@gmail.com>
2018-07-29 20:39:53 +0530

    commit

--- python/test.py
+++ python/test.py
@@ -1,27 +1,26 @@
-from __future__ import print_function
-class ClassTypeerror(RuntimeError):
-	def __init__(self, arg):
-		self.args = arg
-	
-class A():
-	def __init__(self):
-		self.foo = 'foo'
+from pprint import pprint
+# with open('p059_cipher.txt','r') as f:
+#     content=f.read()
+#     arr =list(map(int,content.split(',')))
+#     #pprint(arr)
+#     for i in range(ord('a'),ord('z')+1):
+#         for j in range(ord('a'),ord('z')+1):
+#             for k in range(ord('a'),ord('z')+1):
+#                 temp =[0]*len(arr)
+#                 #print(i,j,k)
+#                 for index in range(0,len(arr),3):
+#                     if(index<len(arr)):
+#                         temp[index] = arr[index]^i
+#                     if(index+1<len(arr)):
+#                         temp[index+1] = arr[index+1]^j
+#                     if(index+2<len(arr)):
+#                         temp[index+2] = arr[index+2]^k
+#                 print(''.join(map(chr,temp)))
 
-class B():
-	def __init__(self, a):
-		try:
-			if not isinstance(a,A):
-				self.a = a
-			else:
-				raise ClassTypeerror('This is not correct parameter')
+with open('required.txt','r') as  f:
+    content=f.read()
+    s=0
+    for i in content:
+        s+=ord(i)
+    print (s)
 
-		except ClassTypeerror :
-		
-			raise
-
-
-
-
-a = A()
-B(a)    # right
-B('a')  # wrong
