daff430aeceb6788e85ba73ce9717e6f4bb52e0d
albinpaul <albinpaul.ks@gmail.com>
2018-07-29 20:35:12 +0530

    changedpythonfile

--- python/test.py
+++ python/test.py
@@ -1,1 +1,27 @@
-s={1,2,3}
+from __future__ import print_function
+class ClassTypeerror(RuntimeError):
+	def __init__(self, arg):
+		self.args = arg
+	
+class A():
+	def __init__(self):
+		self.foo = 'foo'
+
+class B():
+	def __init__(self, a):
+		try:
+			if not isinstance(a,A):
+				self.a = a
+			else:
+				raise ClassTypeerror('This is not correct parameter')
+
+		except ClassTypeerror :
+		
+			raise
+
+
+
+
+a = A()
+B(a)    # right
+B('a')  # wrong
