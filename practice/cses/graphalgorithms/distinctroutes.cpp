defb0df6eae8af39429b6802a5bc3e6b0f823da3
Albin Paul <albinpaul.ks@gmail.com>
2021-01-03 21:13:44 +0530

    completed graph algorithms

---
+++ practice/cses/graphalgorithms/distinctroutes.cpp
@@ -0,0 +1,225 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+ 
+string to_string(const string &s) {
+  return '"' + s + '"';
+}
+ 
+string to_string(const char *s) {
+  return to_string((string)s);
+}
+ 
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+ 
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+ 
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+ 
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+ 
+#define range(i, n) for (long i = 0; i < (long long)n; ++i)
+#define rrange(i, n) for (long i = (long long)n - 1; i >= 0; --i)
+#define all(x) x.begin(), x.end()
+#define watch(x) cerr << #x << " is : " << x << " "
+using pii = pair<long, long>;
+using vi = vector<long>;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1e9 + 7;
+const ll LLMAX = numeric_limits<ll>::max();
+const ll LMAX = numeric_limits<long>::max();
+ 
+ 
+using ele = array<ll, 2>;
+using ele_v = vector<ele>;
+using ele_vv = vector<vector<ele>>;
+using ll_v = vector<ll>;
+using ll_vv = vector<vector<ll>>;
+using ll_s = stack<ll>;
+using bool_v = vector<bool>;
+ 
+template <typename D, typename V>
+void add(D &data, V value) {
+  data = (data + value)%MOD;
+}
+const long int N = 503, M = 1000; 
+ll graph[N][N];
+bool visited[N];
+ll parent[N];
+ll n, m;
+ll edges[2 * N * N + 5];
+vector <ll> ans;
+ 
+bool bfs(ll start, ll end) {
+  queue <int> q;
+  q.push(start);
+  memset(visited, 0, sizeof(visited));
+  visited[start] = 1;
+  while(q.size()) {
+    ll u = q.front();
+    q.pop();
+    range(i, n) {
+      debug(u, i, graph[u][i], edges[graph[u][i]]);
+      if (!visited[i] && graph[u][i] >= 0 && edges[graph[u][i]]) {
+        debug(u, i, graph[u][i], edges[graph[u][i]]);
+        parent[i] = u;
+        visited[i] = 1;
+        q.push(i);
+      }
+    }
+  }
+  return visited[end];
+}
+
+void dfs2(ll u=0) {
+  ans.push_back(u);
+  range(i, n) {
+    if(graph[u][i]%2==0 && !edges[graph[u][i]]) {
+      dfs2(i);
+      edges[graph[u][i]] = 1;
+      return;
+    }
+  }
+} 
+void ford_fulkerson()
+{
+    ll max_flow = 0;
+    while(bfs(0, n-1)) {
+      ll path_flow = LLMAX;
+      ll degrees = LLMAX;
+      vector <ll> temp;
+      for(ll v= n-1 ; v!= 0; v=parent[v]) {
+        ll u = parent[v];
+        if (path_flow > edges[graph[u][v]]) {
+          path_flow = edges[graph[u][v]];
+        }
+      }
+      for(ll v= n-1 ; v!= 0; v=parent[v]) {
+        ll u = parent[v];
+        edges[graph[u][v]] -= path_flow;
+        edges[graph[v][u]] += path_flow;    
+      }
+      max_flow += path_flow;
+    }
+    cout << max_flow << '\n';
+    range(i, max_flow) {
+      dfs2();
+      cout << ans.size() << '\n';
+      debug(ans);
+      for(auto i: ans) {
+        cout << i + 1 << ' ';
+      }
+      cout << '\n';
+      ans.clear();
+    }
+}
+ 
+ 
+ 
+void init() {
+  memset(graph, -1, sizeof(graph));
+  cin >> n >> m;
+  range(i, m) {
+    ll a, b, c;
+    cin >> a >> b;
+    --a; --b;
+    graph[a][b] = 2*i;
+    if (graph[b][a] == -1)
+      graph[b][a] = 2*i + 1;
+    edges[graph[a][b]] += 1;
+    // debug(a, b, graph[a][b], graph[b][a], edges[graph[a][b]]);
+  }
+  ford_fulkerson();
+ 
+}
+ 
+void solve() {
+ 
+}
+ 
+int main(void) {
+  #ifdef LOCAL
+  freopen("in", "r", stdin); 
+  #endif
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  init();
+  return 0;
+  cin >> t;
+  for (int tt = 0; tt < t; tt++) {
+    solve();
+  }
+  return 0;
+}
---
+++ practice/cses/graphalgorithms/schooldance.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string &s) {
+  return '"' + s + '"';
+}
+
+string to_string(const char *s) {
+  return to_string((string)s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+#define range(i, n) for (long i = 0; i < (long long)n; ++i)
+#define rrange(i, n) for (long i = (long long)n - 1; i >= 0; --i)
+#define all(x) x.begin(), x.end()
+#define watch(x) cerr << #x << " is : " << x << " "
+using pii = pair<long, long>;
+using vi = vector<long>;
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1e9 + 7;
+const ll LLMAX = numeric_limits<ll>::max();
+const ll LMAX = numeric_limits<long>::max();
+
+
+using ele = array<ll, 2>;
+using ele_v = vector<ele>;
+using ele_vv = vector<vector<ele>>;
+using ll_v = vector<ll>;
+using ll_vv = vector<vector<ll>>;
+using ll_s = stack<ll>;
+using bool_v = vector<bool>;
+
+template <typename D, typename V>
+void add(D &data, V value) {
+  data = (data + value)%MOD;
+}
+const long int N = 503, M = 1000; 
+bool visited[N];
+vector<ll> adj[N];
+ll n, m, k;
+ll match[N];
+
+bool dfs(int u) {
+  visited[u] = 1;
+  for(auto v: adj[u]) {
+    if(match[v] < 0 || !visited[match[v]] && dfs(match[v]) ) {
+      match[v] = u;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void init() {
+  memset(match, -1, sizeof(match));
+  cin >> n >> m >> k;
+  range(i, k) {
+    ll a, b, c;
+    cin >> a >> b;
+    --a; --b;
+    adj[a].push_back(b);
+  }
+  ll flow = 0;
+  range(i, n) {
+    memset(visited, 0, sizeof(visited));
+    debug(i);
+    flow += dfs(i);
+  }
+  cout << flow << '\n';
+  range(i, m) {
+    if (match[i] >=0) {
+      cout << match[i] + 1 << " " << i + 1 << "\n"; 
+    }
+  } 
+    
+}
+
+void solve() {
+
+}
+
+int main(void) {
+  #ifdef LOCAL
+  freopen("in", "r", stdin); 
+  #endif
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  init();
+  return 0;
+  cin >> t;
+  for (int tt = 0; tt < t; tt++) {
+    solve();
+  }
+  return 0;
+    // cout << "Case #" << tt + 1 << ": ";
+}
